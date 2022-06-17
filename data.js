8c3c40ad035e6a0452880c0a7fbe88bbc0481b7e
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2022-06-17 07:35:05 -0400

    init

---
+++ data.js
@@ -0,0 +1,135 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-area-1",
+      "name": "Area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 0.03087890215067013,
+        "pitch": 0.3670923637797383,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10464861154689586,
+          "pitch": 0.3621888177732373,
+          "rotation": 0,
+          "target": "1-area-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5963655843184448,
+          "pitch": 1.1627578092178688,
+          "title": "Pond 001",
+          "text": "Pond is fully constructed and certified."
+        },
+        {
+          "yaw": 0.6434173836927055,
+          "pitch": 0.03191827755923171,
+          "title": "Power Line",
+          "text": "Power Line Right of Way"
+        },
+        {
+          "yaw": -0.19818806906546982,
+          "pitch": 0.30333584724544416,
+          "title": "Hagy Seam",
+          "text": "Outcrop uncovered."
+        }
+      ]
+    },
+    {
+      "id": "1-area-2",
+      "name": "Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7019244871329917,
+          "pitch": -0.334885856869235,
+          "rotation": 0,
+          "target": "0-area-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.613852190150503,
+          "pitch": 0.17570999504234308,
+          "title": "Hagy Seam",
+          "text": "Outcrop uncovered."
+        },
+        {
+          "yaw": 1.6354273226894378,
+          "pitch": 0.05910070935050982,
+          "title": "Power Line",
+          "text": "Power Line Right of Way being cleared."
+        }
+      ]
+    }
+  ],
+  "name": "Shop Branch 6-15-22",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
